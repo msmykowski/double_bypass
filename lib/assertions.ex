2a030dd50bb8ca2bd50932f0727ff447336d533b
Michael Smykowski <msmykowski@bleacherreport.com>
2017-05-12 17:51:21 -0700

    refactor assertion module

--- lib/assertions.ex
+++ lib/assertions.ex
@@ -1,56 +1,36 @@
 defmodule DoubleBypass.Assertions do
   use ExUnit.CaseTemplate
 
-  def run(conn, opts) do
-    assert_headers(conn, opts)
-    assert_path(conn, opts)
-    assert_query(conn, opts)
-    assert_method(conn, opts)
-    assert_body(conn, opts)
-    response(conn, opts)
+  def run(conn, params) do
+    Enum.map(params, &assert_on(conn, &1))
+    send_resp(conn, params)
   end
 
-  defp assert_path(conn, %{path: path}), do: assert conn.request_path == path
-  defp assert_path(_conn, _params), do: :noop
-
-  defp assert_query(conn, %{query: query}), do: assert conn.query_string == query
-  defp assert_query(_conn, _params), do: :noop
-
-  defp assert_method(conn, %{method: method}), do: assert conn.method == method
-  defp assert_method(_conn, _params), do: :noop
-
-  defp assert_headers(conn, %{headers: headers}) do
+  defp assert_on(conn, {:path, path}), do: assert conn.request_path == path
+  defp assert_on(conn, {:query, query}), do: assert conn.query_string == query
+  defp assert_on(conn, {:method, method}), do: assert conn.method == method
+  defp assert_on(conn, {:headers, headers}) do
     conn_headers = conn.req_headers |> Enum.into(%{})
     Enum.map(headers, fn({k, v}) ->
       assert conn_headers[k] == v
     end)
   end
-  defp assert_headers(_conn, _params), do: :noop
-
-  defp assert_body(conn, %{body: body}) do
+  defp assert_on(conn, {:body, body}) do
     assert conn
       |> Plug.Conn.read_body
       |> elem(1)
       |> Poison.decode! == body
   end
-  defp assert_body(_conn, _params), do: :noop
+  defp assert_on(conn, _), do: :noop
 
-  defp response(conn, %{response: response, status_code: status_code}) when is_bitstring(response) do
-    Plug.Conn.resp(conn, status_code, response)
-  end
-  defp response(conn, %{response: response, status_code: status_code}) do
-    Plug.Conn.resp(conn, status_code, Poison.encode!(response))
-  end
-  defp response(conn, %{status_code: status_code}) do
-    Plug.Conn.resp(conn, status_code, "")
-  end
-  defp response(conn, %{response: response}) when is_bitstring(response) do
-    Plug.Conn.resp(conn, 200, response)
-  end
-  defp response(conn, %{response: response}) do
-    Plug.Conn.resp(conn, 200, Poison.encode!(response))
-  end
-  defp response(conn, _params) do
-    Plug.Conn.resp(conn, 200, "")
+  defp send_resp(conn, params) do
+    case params do
+       %{response: response, status_code: status_code} when is_bitstring(response) -> Plug.Conn.resp(conn, status_code, response)
+       %{response: response, status_code: status_code} -> Plug.Conn.resp(conn, status_code, Poison.encode!(response))
+       %{status_code: status_code} -> Plug.Conn.resp(conn, status_code, "")
+       %{response: response} when is_bitstring(response) -> Plug.Conn.resp(conn, 200, response)
+       %{response: response} -> Plug.Conn.resp(conn, 200, Poison.encode!(response))
+       _ -> Plug.Conn.resp(conn, 200, "")
+    end
   end
 end
