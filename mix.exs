408780d372bc572900346a87eb29f27ea01b2bbe
Michael Smykowski <msmykowski@bleacherreport.com>
2017-05-13 13:26:59 -0700

    add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -3,11 +3,12 @@ defmodule DoubleBypass.Mixfile do
 
   def project do
     [app: :double_bypass,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
+     package: package(),
      test_coverage: [tool: ExCoveralls]]
   end
 
@@ -21,6 +22,23 @@ defmodule DoubleBypass.Mixfile do
       {:excoveralls, "~> 0.6", only: :test},
       {:httpoison, "~> 0.11.1", only: :test},
       {:poison, "~> 2.0"}
+    ]
+  end
+
+  defp description do
+    """
+    A wrapper around Bypass. Double Bypass makes configuring and initializing Bypass easy.
+    It also removes redundant test assertion code, and ecourages testing external requests in
+    a self documenting manner.
+    """
+  end
+
+  defp package do
+    [
+      name: :double_bypass,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Michael Smykowski"],
+      links: %{"GitHub" => "https://github.com/msmykowski/double_bypass"}
     ]
   end
 end
